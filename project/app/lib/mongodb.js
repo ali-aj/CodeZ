af96bc0fcbeca6911769e5973d389643abe3f14b
Mustafa Mansoor <mustafamansoor47@gmail.com>
2025-01-25 01:57:41 +0500

    mongodb setup done

---
+++ project/app/lib/mongodb.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+global.mongoose = {
+  conn: null,
+  promise: null,
+};
+
+export async function dbConnect() {
+  try {
+    if (global.mongoose && global.mongoose.conn) {
+      console.log("Connected from previous");
+      return global.mongoose.conn;
+    } else {
+      // isko env ma dalna ha 
+      const conString ="mongodb+srv://f219165:HHH9165HHH@cluster0.bvudl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+      const promise = mongoose.connect(conString, {
+        autoIndex: true,
+      });
+
+      global.mongoose = {
+        conn: await promise,
+        promise,
+      };
+
+      console.log("Newly connected");
+      return await promise;
+    }
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw new Error("Database connection failed");
+  }
+}
+
+export const disconnect = () => {
+  if (!global.mongoose.conn) {
+    return;
+  }
+  global.mongoose.conn = null;
+  mongoose.disconnect();
+};
